3e4e30cd862249cbb557315aeb310b8c0b237c29
lkaimin <lkm940213@gmail.com>
2020-05-05 12:09:46 +0800

    week03

---
+++ Week_03/lowest_common_ancestor_of_a_binary_tree.go
@@ -0,0 +1,14 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+	  return root
+  }
+  l := lowestCommonAncestor(root.Left, p, q)
+  r := lowestCommonAncestor(root.Right, p, q)
+  if l == nil {
+	  return r
+  }
+  if r == nil {
+	  return l
+  }
+  return root
+}
